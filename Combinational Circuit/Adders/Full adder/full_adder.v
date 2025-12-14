bce1e8acfb65c579b563132b892209dde8f3b65f
GANESH S <165929138+Captain-VLSI@users.noreply.github.com>
2025-12-14 13:38:01 +0530

    Update full_adder.v

--- Combinational Circuit/Adders/Full adder/full_adder.v
+++ Combinational Circuit/Adders/Full adder/full_adder.v
@@ -1,3 +1,19 @@
+module half_adder (
+    input  wire a,
+    input  wire b,
+    output wire sum,
+    output wire carry
+);
+
+    assign sum   = a ^ b;
+    assign carry = a & b;
+
+endmodule
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+`include "half adder.sv"
 module full_adder (
     input  wire a,
     input  wire b,
