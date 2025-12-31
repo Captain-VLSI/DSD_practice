8ae5d0dc41f451b5bd5a3026da9b16a5cd2eea70
GANESH S <165929138+Captain-VLSI@users.noreply.github.com>
2025-12-31 09:22:47 +0530

    Create demux_1_4.v
    Design of 1_4 demux using data flow

---
+++ Combinational Circuit/Demultiplexers/1_4 demux/demux_1_4.v
@@ -0,0 +1,8 @@
+module demux_1_4 (input I,
+        input [1:0]S,
+        output [3:0]Y);
+  assign Y[0] = ~S[1] & ~S[0] & I;
+  assign Y[1] = ~S[1] & S[0] & I;
+  assign Y[2] = S[1] & ~S[0] & I;
+  assign Y[3] = S[1] & S[0] & I;
+endmodule
