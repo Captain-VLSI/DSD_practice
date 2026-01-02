6d407b64c2964cf34fe9e5830706c86c64f2deef
GANESH S <165929138+Captain-VLSI@users.noreply.github.com>
2026-01-02 23:54:56 +0530

    Update half_adder_nor.sv

--- Combinational Circuit/Adders/Half adder using NOR gate only/half_adder_nor.sv
+++ Combinational Circuit/Adders/Half adder using NOR gate only/half_adder_nor.sv
@@ -1,1 +1,22 @@
-module
+module half_adder_nor (
+    input wire a,
+    input wire b,
+    output wire sum,
+    output wire carry
+);
+
+    wire not_a;
+    wire not_b;
+    wire a_nor_b;
+
+    nor g1 (not_a, a, a); 
+
+    nor g2 (not_b, b, b);
+
+    nor g3 (carry, not_a, not_b);
+
+    nor g4 (a_nor_b, a, b);
+  
+    nor g5 (sum, a_nor_b, carry);
+
+endmodule
